790d6d2517a76bcbb6e5092d57cbdd390eab7b34
NguyenMinhAnh-CDTH18PMC <0306181197@caothang.edu.vn>
2020-12-26 00:04:27 +0700

    tạo trang thông tin chi tiết phim

--- Cinema/app/src/main/java/com/example/cinema/ui/CardChiTietFragment.java
+++ Cinema/app/src/main/java/com/example/cinema/ui/CardChiTietFragment.java
@@ -8,13 +8,25 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cinema.R;
+import com.example.cinema.adapters.ActorAdapter;
+import com.example.cinema.adapters.DienVien_DaoDienAdapter;
+import com.example.cinema.models.Actor;
+
+import java.util.LinkedList;
 
 public class CardChiTietFragment  extends Fragment {
     private static final String ARG_COUNT = "param1";
     private static Integer counter;
     View rootView;
+
+    RecyclerView rvDienVien, rvDaoDien;
+
+    LinkedList<Actor> actorlist= new LinkedList<Actor>();
+    LinkedList<Actor> directorlist= new LinkedList<Actor>();
 
     public CardChiTietFragment() {
     }
@@ -40,7 +52,27 @@ public class CardChiTietFragment  extends Fragment {
                 rootView= inflater.inflate(R.layout.chitiet_datve, container, false);
                 break;
             case 1:
-                rootView= inflater.inflate(R.layout.activity_chi_tiet_phim, container, false);
+                rootView= inflater.inflate(R.layout.chi_tiet_thongtin, container, false);
+
+                rvDienVien= rootView.findViewById(R.id.rvDienVien);
+                rvDaoDien= rootView.findViewById(R.id.rvDaoDien);
+
+                actorlist.add(new Actor(R.drawable.dienvien1, "Minh Hai"));
+                actorlist.add(new Actor(R.drawable.dienvien1, "Minh Hai"));
+                actorlist.add(new Actor(R.drawable.dienvien1, "Minh Hai"));
+
+                directorlist.add(new Actor(R.drawable.dienvien1, "Minh Hai"));
+                directorlist.add(new Actor(R.drawable.dienvien1, "Minh Hai"));
+                directorlist.add(new Actor(R.drawable.dienvien1, "Minh Hai"));
+
+                DienVien_DaoDienAdapter actorAdapter = new DienVien_DaoDienAdapter(getContext(), actorlist);
+                rvDienVien.setAdapter(actorAdapter);
+                rvDienVien.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
+
+                DienVien_DaoDienAdapter directorAdapter = new DienVien_DaoDienAdapter(getContext(), actorlist);
+                rvDaoDien.setAdapter(directorAdapter);
+                rvDaoDien.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
+
                 break;
             case 2:
                 rootView= inflater.inflate(R.layout.chitiet_tintuc, container, false);
